e57fd8ed41ffd4225ae16d4bcb904a92d1d9d3a2
David Salgado <david.salgado@digital.justice.gov.uk>
2016-10-26 15:08:46 +0100

    pass @params through to the POST
    Instead of specifying every allowed parameter,
    pass through whatever is provided. GLiMR should
    simply ignore any parameters it doesn't want, so
    this should not introduce any new vulnerabilities.

--- lib/glimr_api_client/register_new_case.rb
+++ lib/glimr_api_client/register_new_case.rb
@@ -12,7 +12,7 @@ module GlimrApiClient
         :jurisdictionId,
         :onlineMappingCode
       ].each do |required|
-        errors << required if request_body.fetch(required).blank?
+        errors << required if request_body.key?(required).blank?
       end
       raise RequestError, errors unless errors.blank?
     end
@@ -22,35 +22,7 @@ module GlimrApiClient
     end
 
     def request_body
-      {
-        contactPhone: @params[:contactPhone],
-        contactFax: @params[:contactFax],
-        contactEmail: @params[:contactEmail],
-        contactPreference: @params[:contactPreference],
-        contactFirstName: @params[:contactFirstName],
-        contactLastName: @params[:contactLastName],
-        contactStreet1: @params[:contactStreet1],
-        contactStreet2: @params[:contactStreet2],
-        contactStreet3: @params[:contactStreet3],
-        contactStreet4: @params[:contactStreet4],
-        contactCity: @params[:contactCity],
-        jurisdictionId: @params[:jurisdictionId],
-        onlineMappingCode: @params[:onlineMappingCode],
-        documentsURL: @params[:documentsURL],
-        repPhone: @params[:repPhone],
-        repFax: @params[:repFax],
-        repEmail: @params[:repEmail],
-        repPreference: @params[:repPreference],
-        repReference: @params[:repReference],
-        repIsAuthorised: @params[:repIsAuthorised],
-        repOrganisationName: @params[:repOrganisationName],
-        repFAO: @params[:repFAO],
-        repStreet1: @params[:repStreet1],
-        repStreet2: @params[:repStreet2],
-        repStreet3: @params[:repStreet3],
-        repStreet4: @params[:repStreet4],
-        repCity: @params[:repCity]
-      }
+      @params
     end
   end
 end
