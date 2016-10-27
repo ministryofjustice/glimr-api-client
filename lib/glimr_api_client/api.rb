3792b6f60adbd40cdeb00bee20ff1b57b0a3affe
Todd Tyree <tatyree@gmail.com>
2016-10-27 11:17:39 +0100

    Prevent integration tests from breaking
    The `DEFAULT_URL` constant wasn't picking up the value of
    `GLIMR_API_URL` when the gem was being run in integration tests in
    `tax-tribunals-fees`.  This was because the `dotenv` gem had to be
    loaded and required prior to the `glimr-api-client` gem. In order to
    avoid doing this, I have changed the constant to a method that fetches
    the value when it is called.

--- lib/glimr_api_client/api.rb
+++ lib/glimr_api_client/api.rb
@@ -4,14 +4,8 @@ require 'active_support/core_ext/object/to_query'
 
 module GlimrApiClient
   module Api
-    DEFAULT_ENDPOINT =
-      ENV.fetch(
-        'GLIMR_API_URL',
-        'https://glimr-api.taxtribunals.dsd.io/Live_API/api/tdsapi'
-    )
-
     def post
-      client("#{DEFAULT_ENDPOINT}#{endpoint}").post(body: request_body.to_query).tap { |resp|
+      client("#{api_url}#{endpoint}").post(body: request_body.to_query).tap { |resp|
         # Only timeouts and network issues raise errors.
         handle_response_errors(resp)
         @body = resp.body
@@ -25,6 +19,14 @@ module GlimrApiClient
     end
 
     private
+
+    # If this is set using a constant, and the gem is included in a project
+    # that uses the dotenv gem, then it will always fall through to the default
+    # unless dotenv is included and required before this gem is loaded.
+    def api_url
+      ENV.fetch('GLIMR_API_URL',
+                'https://glimr-api.taxtribunals.dsd.io/Live_API/api/tdsapi')
+    end
 
     def handle_response_errors(resp)
       if (!endpoint.eql?('/paymenttaken') && resp.status.equal?(404))
@@ -49,10 +51,10 @@ module GlimrApiClient
       Excon.new(
         uri,
         headers: {
-          'Content-Type' => 'application/json',
-          'Accept' => 'application/json'
-        },
-        persistent: true
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      },
+      persistent: true
       )
     end
   end
