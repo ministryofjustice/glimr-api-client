cfdd89221f3d581e539791c4512407181d6b2592
Todd Tyree <tatyree@gmail.com>
2017-01-05 22:41:05 +0000

    Fix infrastructure issues
    https://www.pivotaltracker.com/story/show/136552831

---
+++ spec/lib/available_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe GlimrApiClient::Available do
+
+  it 'posts and empty hash as its payload' do
+    req = double(:request, body: {})
+    resp = double(:response).as_null_object
+    expect(req).to receive(:post).with(body: '{}').and_return(resp)
+    allow(subject).to receive(:client).and_return(req)
+    expect { subject.call }.not_to raise_error
+  end
+end
